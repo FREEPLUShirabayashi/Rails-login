3400a0581d5ccd2196b0677a7b725eec9bf7bb0f
FREEPLUShirabayashi <hirabayashi_sena@freeplus.co.jp>
2018-09-18 09:25:06 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # post '/microposts' to 'microposts#'
-  resources :microposts
-  resources :users
-  root 'users#index'
-  root 'application#hello'
+  # resources :microposts
+  # resources :users
+  # root 'users#index'
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+  get  'static_pages/contact'
+  # root 'application#hello'
 end
